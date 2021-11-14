86d6de7115cc29026537ffa587c8a444444e7c32
Jean-Michel HA <jean-michel.ha@edu.esiee.fr>
2021-11-14 17:52:19 +0100

    update xml pour la partie projet

--- app/src/main/java/com/example/student_to_do_list/ui/dashboard/Project_Activity.java
+++ app/src/main/java/com/example/student_to_do_list/ui/dashboard/Project_Activity.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class Project_Activity extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public Project_Activity() {
         mText = new MutableLiveData<>();
         mText.setValue("No ongoing project");
     }
