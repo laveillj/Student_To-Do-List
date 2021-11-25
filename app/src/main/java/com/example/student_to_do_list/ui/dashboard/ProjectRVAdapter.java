291a2d947d93a55a0d22fceabd9ba362b7e4826e
Jean-Michel HA <jean-michel.ha@edu.esiee.fr>
2021-11-25 10:09:35 +0100

    Recycler view for project onglet

---
+++ app/src/main/java/com/example/student_to_do_list/ui/dashboard/ProjectRVAdapter.java
@@ -0,0 +1,59 @@
+package com.example.student_to_do_list.ui.dashboard;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ClipData;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.student_to_do_list.R;
+
+import java.util.Collections;
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+
+
+public class ProjectRVAdapter extends RecyclerView.Adapter<ProjectRVAdapter.ItemViewHolder2> {
+
+   List<String> dataList;
+
+   public class ItemViewHolder2 extends RecyclerView.ViewHolder {
+       TextView txt;
+       //TextView deadline;
+
+       public ItemViewHolder2(View itemView) {
+           super(itemView);
+           txt = (TextView) itemView.findViewById(R.id.fragment_tasks_item_title);
+           //deadline = (TextView) itemView.findViewById(R.id.deadline_project);
+       }
+   }
+
+   public ProjectRVAdapter(List<String> dataList) { this.dataList = dataList; }
+
+   @Override
+   public ItemViewHolder2 onCreateViewHolder(ViewGroup parent, int viewType) {
+
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_tasks_item, parent, false);
+
+       return new ItemViewHolder2(view);
+   }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder2 holder, int position) {
+        holder.txt.setText(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+
+}
