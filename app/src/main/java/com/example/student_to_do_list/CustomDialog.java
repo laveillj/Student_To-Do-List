dc3c677c6e68434e6b82f6f3b77509cf25df0614
laveillj <jeremy.laveille@edu.esiee.fr>
2021-12-17 09:36:01 +0100

    Ajout drawable notif de cancel

---
+++ app/src/main/java/com/example/student_to_do_list/CustomDialog.java
@@ -0,0 +1,50 @@
+package com.example.student_to_do_list;
+
+import android.accessibilityservice.AccessibilityService;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CustomDialog extends Dialog {
+
+    private Button buttonCancel;
+    public Context mContext;
+    private Task mTask;
+    DatabaseHelper db;
+
+    public CustomDialog(Context pContext, Task pTask) {
+        super(pContext);
+        this.mContext = pContext;
+        this.mTask = pTask;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_cancel);
+
+        db = new DatabaseHelper(getContext());
+
+        this.buttonCancel  = (Button) findViewById(R.id.button_cancel);
+
+        this.buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonCancelClick();
+            }
+        });
+    }
+
+    // User click "Cancel" button.
+    private void buttonCancelClick()  {
+        db.createTask(mTask);
+        this.dismiss();
+    }
+}
